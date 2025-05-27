3a2ccc8cfd18d463029255db58135674dedb0782
utaliesin <121281158+utaliesin@users.noreply.github.com>
2025-05-27 12:26:52 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,105 @@
+export const CONFIG = {
+	name: "Richard",
+	surname: "Sanchez",
+	position: "Marketing Manager",
+	image: "https://0.gravatar.com/avatar/84059b07d4be67b806386c0aad8070a23f18836bbaae342275dc0a83414c32ee",
+	contact: {
+		number: "+573178213456",
+		email: "richard.sanchez@gmail.com",
+		address: "Calle 123 # 45-67",
+		website: "https://richard.com",
+	},
+	education: [
+		{
+			name: "Wardiere University",
+			degree: "Bachelor of Business",
+			start: "2025",
+			end: "2029",
+			gpa: "3.8",
+		},
+		{
+			name: "Wardiere University",
+			degree: "Master of Business",
+			start: "2029",
+			end: "2030",
+		},
+	],
+	skills: [
+		"Project Management",
+		"Public Relations",
+		"Teamwork",
+		"Time Management",
+		"Leadership",
+		"Effective Communication",
+		"Critical Thinking",
+	],
+	languages: [
+		{
+			name: "English",
+			level: "Fluent",
+		},
+		{
+			name: "French",
+			level: "Fluent",
+		},
+		{
+			name: "German",
+			level: "Basic",
+		},
+		{
+			name: "Spanish",
+			level: "Intermediate",
+		},
+	],
+	profile:
+		"Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum sunt a vero incidunt facere rerum consequatur perspiciatis excepturi? Quo nulla provident ratione officia, amet illo sapiente dolores perspiciatis quod dolor.".repeat(
+			6,
+		),
+	experience: [
+		{
+			company: "Studio Shadwe",
+			position: "Marketing Manager & Specialist",
+			start: "2024",
+			end: "2025",
+			tasks: [
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum sunt a vero incidunt facere rerum consequatur perspiciatis excepturi? Quo nulla provident ratione officia, amet illo sapiente dolores perspiciatis quod dolor.",
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum sunt a vero incidunt facere rerum consequatur perspiciatis excepturi? Quo nulla provident ratione officia, amet illo sapiente dolores perspiciatis quod dolor.",
+			],
+		},
+		{
+			company: "Fauget Studio",
+			position: "Marketing Manager & Specialist",
+			start: "2025",
+			end: "2029",
+			tasks: [
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum sunt a vero incidunt facere rerum consequatur perspiciatis excepturi? Quo nulla provident ratione officia, amet illo sapiente dolores perspiciatis quod dolor.",
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum sunt a vero incidunt facere rerum consequatur perspiciatis excepturi? Quo nulla provident ratione officia, amet illo sapiente dolores perspiciatis quod dolor.",
+			],
+		},
+		{
+			company: "Borcelle Studio",
+			position: "Marketing Manager & Specialist",
+			start: "2030",
+			end: "Present",
+			tasks: [
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum sunt a vero incidunt facere rerum consequatur perspiciatis excepturi? Quo nulla provident ratione officia, amet illo sapiente dolores perspiciatis quod dolor.",
+			],
+		},
+	],
+	references: [
+		{
+			name: "Estelle Darcy",
+			position: "CTO",
+			company: "Wardiere Inc.",
+			phone: "+573178213456",
+			email: "estelle.darcy@wardiere.com",
+		},
+		{
+			name: "Harper Richard",
+			position: "CEO",
+			company: "Wardiere Inc.",
+			phone: "+573178213456",
+			email: "harper.richard@wardiere.com",
+		},
+	],
+};
